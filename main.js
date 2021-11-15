e1bb1fff11fdb2c273bfa549b1a3904ace91d99a
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-11-15 11:18:47 -0500

    modified js/html/css

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 const form = document.querySelector("form")
-const list = document.querySelector("#list")
-const remove = document.querySelector("#remove")
+const addToList = document.querySelector("#list")
+const add = document.querySelector("#add")
+const removeSome = document.querySelector("#removeSome")
 const removeAll = document.querySelector("#removeAll")
 const todoList  = document.querySelector("#todoList")
 
-form.addEventListener('submit', e => {
+add.addEventListener('click', e => {
     e.preventDefault()
     const newItem = document.createElement("p")
     newItem.id = "pTag"
-    newItem.innerHTML = `${list.value}`
+    newItem.innerHTML = `${addToList.value}`
     todoList.appendChild(newItem)
 })
 
 todoList.addEventListener('click', e => {
     e.target.style.textDecoration = "line-through"
+    e.target.className = "line"
+})
+
+removeSome.addEventListener('click', e => {
+    e.preventDefault()
+    const pTag = document.querySelectorAll(".line")
+    pTag.forEach(element => {
+        element.remove()
+    })
+})
+
+removeAll.addEventListener('click', e => {
+    e.preventDefault()
+    const pTag = document.querySelectorAll("#todoList p")
+    console.log(pTag)
+    pTag.forEach(element => {
+        element.remove()
+    })
 })
